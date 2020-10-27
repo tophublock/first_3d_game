d553f336f6dbae8dfe370854adc46ae66bd3476b
Aim4 <aimforking@gmail.com>
2020-10-27 13:58:51 -0400

    Connect start game signal

---
+++ src/UserInterface/MainMenu.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public class MainMenu : Control
+{
+    public override void _Ready()
+    {
+
+    }
+
+//  public override void _Process(float delta)
+//  {
+//
+//  }
+
+    public void OnStartPressed()
+    {
+        GetTree().ChangeScene("res://src/Levels/Level.tscn");
+    }
+}
