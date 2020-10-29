656a44a69bda8fac7938875a981f0eb29cf71038
Aim4 <aimforking@gmail.com>
2020-10-29 12:55:15 -0400

    Implement play again button on game over screen

---
+++ src/UserInterface/GameOver.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class GameOver : MarginContainer
+{
+
+    public override void _Ready()
+    {
+
+    }
+
+//  public override void _Process(float delta)
+//  {
+//
+//  }
+
+    public void OnPlayAgainPressed()
+    {
+        GetTree().ChangeScene("res://src/Levels/Level.tscn");
+    }
+}
