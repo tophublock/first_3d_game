e4af82be169415a128772c0be870b2c45dfec230
Aim4 <aimforking@gmail.com>
2020-10-29 13:34:54 -0400

    Add idle rotation to coin

---
+++ src/Objects/Coin.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class Coin : Area
+{
+    public float RotationSpeed = 3;
+    public override void _Ready()
+    {
+
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        this.Transform = new Transform(
+            this.Transform.basis.Rotated(Vector3.Up, DegToRad(RotationSpeed)),
+            this.Transform.origin
+        );
+    }
+    private float DegToRad(float degrees)
+    {
+        return (Mathf.Pi / 180) * degrees;
+    }
+}
